41cf4bc7a7deefa0e9892aa2da8ed0ea3f7a8bd9
AngginMPCS <anggin.megantara@pcsindonesia.co.id>
2024-06-19 21:08:17 +0700

    Update Patch v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './assets/css/main.css';
+import Homepage from './view/homepage';
+import ListSearch from './view/listSearch';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/results" element={<ListSearch />} />
+          {/* <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
